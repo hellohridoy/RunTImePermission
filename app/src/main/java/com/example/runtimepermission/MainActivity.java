64c6d653fbdadbaedf0b8810a9816e10430e8de9
Hridoy Hossain <ridoy.java@gmail.com>
2022-02-16 12:49:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/runtimepermission/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.runtimepermission;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button permission;
+    String permissionArray[]={"android.permission.READ_EXTERNAL_STORAGE","android.permission.CALL_PHONE"};
+    int code = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        permission=findViewById(R.id.permissionButton);
+        permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(permissionArray,code);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==code){
+            if (grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(), "permission granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "permission deny", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+}
